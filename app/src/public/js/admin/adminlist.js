4cdbfbe318e19516658ab8d1736c4db49daca4f4
Galdong <utcmaster@naver.com>
2024-06-28 16:43:35 +0900

    관리자페이지 메뉴바 구성변경

--- app/src/public/js/admin/adminlist.js
+++ app/src/public/js/admin/adminlist.js
@@ -9,3 +9,30 @@ function logout() {
     } else {
     }
 }
+document.addEventListener('DOMContentLoaded', function() {
+    var dropdownBtns = document.querySelectorAll('.dropdown-btn');
+
+    dropdownBtns.forEach(function(dropdownBtn) {
+        dropdownBtn.addEventListener('click', function() {
+            dropdownBtns.forEach(function(btn) {
+                if (btn !== dropdownBtn) {
+                    btn.classList.remove('active');
+                    btn.nextElementSibling.style.display = 'none';
+                }
+            });
+            dropdownBtn.classList.toggle('active');
+            var dropdownContent = dropdownBtn.nextElementSibling;
+            dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        dropdownBtns.forEach(function(dropdownBtn) {
+            if (!dropdownBtn.contains(event.target) && !dropdownBtn.nextElementSibling.contains(event.target)) {
+                dropdownBtn.classList.remove('active');
+                dropdownBtn.nextElementSibling.style.display = 'none';
+            }
+        });
+    });
+});
+
